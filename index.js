077d83f4abeb133e2ac5038fc32ed7d3425387c8
sandeep <vagarurusandeep@gmail.com>
2023-11-27 21:53:08 +0530

    changes are saved

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const heading= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},
-[React.createElement("h1",{id:"heading"},"welcome to namaste react sandeep"),React.createElement("h2",{},"hello sandeep")]),
-React.createElement("div",{id:"child2"})]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <header/>
+        </div>
+    )
+}
+
+const header = () => {
+    return (
+        <div className="header">
+        <div className="logo">
+        </div>
+        <img src="online-food-delivery-industry-min.jpg"/>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("react"));
 root.render(heading);
